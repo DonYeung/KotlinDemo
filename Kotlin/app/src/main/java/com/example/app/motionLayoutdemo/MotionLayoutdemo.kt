e4c3970d3439604cf91a86b0f5d97146117d7ace
yangzhendong <yangzhendong@shuixindk.cn>
2020-04-30 18:28:11 +0800

    临时提交

---
+++ Kotlin/app/src/main/java/com/example/app/motionLayoutdemo/MotionLayoutdemo.kt
@@ -0,0 +1,12 @@
+package com.example.app.motionLayoutdemo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.app.R
+
+class MotionLayoutdemo :AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_motionlayout)
+    }
+}
